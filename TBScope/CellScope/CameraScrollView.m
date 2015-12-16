2f1ee135870a8c5c3558ac10e690c7e72d7e1392
Jason Ardell <ardell@gmail.com>
2015-12-16 15:32:44 -0700

    Remove unused shader.

--- TBScope/CellScope/CameraScrollView.m
+++ TBScope/CellScope/CameraScrollView.m
@@ -10,24 +10,6 @@
 #import "TBScopeCamera.h"
 #import <GPUImage/GPUImage.h>
 
-// Alpha mask shader string
-NSString * const kNBUAlphaMaskShaderString = SHADER_STRING
-(
- varying highp vec2 textureCoordinate;
- varying highp vec2 textureCoordinate2;
- 
- uniform sampler2D inputImageTexture;
- uniform sampler2D inputImageTexture2;
- 
- void main()
- {
-     lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
-     lowp vec4 textureColor2 = texture2D(inputImageTexture2, textureCoordinate2);
-     
-     gl_FragColor = vec4(textureColor.xyz, textureColor2.a);
- }
-);
-
 @implementation CameraScrollView {
     GPUImageVideoCamera *videoCamera;
     GPUImageColorMatrixFilter *colorFilter;
